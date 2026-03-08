8fa0bbcc05925d957b6b850d9f3a62d058271999
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-03-09 00:37:04 +0530

    defined order & counter schema

---
+++ server/src/models/orderSchema.js
@@ -0,0 +1,106 @@
+import mongoose from "mongoose";
+import { Counter } from "./counterSchema";
+
+const orderSchema = new mongoose.Schema({
+  orderId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Customer",
+    required: true,
+  },
+  deliveryPartner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "DeliveryPartner",
+  },
+  branch: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Branch",
+    required: true,
+  },
+  items: [
+    {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      count: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  deliveryLocation: {
+    latitude: { type: Number },
+    longitude: { type: Number },
+    address: {
+      type: String,
+    },
+  },
+  pickupLocation: {
+    latitude: { type: Number },
+    longitude: { type: Number },
+    address: {
+      type: String,
+    },
+  },
+  deliveryPartnerLocation: {
+    latitude: { type: Number },
+    longitude: { type: Number },
+    address: {
+      type: String,
+    },
+  },
+  status: {
+    type: String,
+    enum: ["available", "confirmed", "arriving", "delivered", "cancelled"],
+    default: "available",
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+async function generateNextSequenceValue(sequenceName) {
+  const sequenceDocument = await Counter.findOneAndUpdate(
+    {
+      name: sequenceName,
+    },
+    {
+      $inc: { sequence_value: 1 },
+    },
+    {
+      new: true,
+      upsert: true,
+    },
+  );
+  return sequenceDocument.sequence_value;
+}
+
+orderSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const sequenceValue = await generateNextSequenceValue("orderId");
+    this.orderId = `ORDR${sequenceValue.toString().padStart(5, "0")}`;
+  }
+  next;
+});
+
+export const Order = mongoose.model("Order", orderSchema);
