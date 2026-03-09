7abf4f660115c5c46a1e713ff183a3b2b8e31a46
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-03-10 00:00:56 +0530

    export all schema dynamically

---
+++ server/src/models/index.js
@@ -0,0 +1,17 @@
+import { Customer, DeliveryPartner, Admin } from "./userSchema";
+import { Product } from "./productSchema";
+import { Order } from "./orderSchema";
+import { Counter } from "./counterSchema";
+import { Category } from "./categorySchema";
+import { Branch } from "./branchSchema";
+
+export {
+  Customer,
+  DeliveryPartner,
+  Admin,
+  Product,
+  Order,
+  Counter,
+  Category,
+  Branch,
+};
